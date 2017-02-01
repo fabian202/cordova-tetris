22fc5e24764e55b2e8e6af25ce3ded8a7fab352b
Fabian Marin <fabian@conexus202.com>
2017-02-01 16:54:30 -0500

    collisions

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,20 @@ const matrix = [
   [0, 1, 0],
 ];
 
+function collide(arena, player) {
+  //const [m, o] = [player.matrix, player.pos];
+  const m = player.matrix;
+  const o = player.pos;
+  for (let y = 0; y < m.length; y++) {
+    for (let x = 0; x < m[y].length; x++) {
+      if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function createMatrix(w, h) {
   const matrix = [];
   while (h--) {
@@ -28,7 +42,7 @@ function createMatrix(w, h) {
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0,0, canvas.width, canvas.height);
-
+  drawMatrix(arena, {x: 0, y: 0});
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -47,8 +61,6 @@ function merge(arena, player) {
   player.matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if(value) {
-        console.log(y + player.pos.y, x + player.pos.x);
-        console.log(value);
         arena[y + player.pos.y][x + player.pos.x] = value;
       }
     });
@@ -62,11 +74,23 @@ const player = {
 
 function playerDrop() {
   player.pos.y++;
+  if(collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
   dropCounter = 0;
 }
 
+function playerMove(dir) {
+  player.pos.x += dir;
+  if(collide(arena, player)) {
+    player.pos.x -= dir;
+  }
+}
+
 let dropCounter = 0;
-let dropInterval = 2000;
+let dropInterval = 500;
 let lastTime = 0;
 
 function update(time) {
@@ -93,19 +117,16 @@ var hammertime = new Hammer(canvas, { recognizers: [
 
 //Gestos
 hammertime.on('swipeleft', function(ev) {
-	console.log('swip left');
-  player.pos.x--;
+  // player.pos.x--;
+  playerMove(-1);
 });
 //
 hammertime.on('swiperight', function(ev) {
-	console.log('swip right');
-  player.pos.x++;
+  // player.pos.x++;
+  playerMove(1);
 });
 
 hammertime.on('swipedown', function(ev) {
-	console.log('swip down');
-  player.pos.y++;
-  dropCounter = 0;
   playerDrop();
 });
 
