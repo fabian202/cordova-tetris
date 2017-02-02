09e0581a136d3001bab763f79ecedf3b2ca99502
Fabian Marin <fabian-guitar@hotmail.com>
2017-02-01 23:52:50 -0500

    pieces colors, lines and game over

--- www/js/index.js
+++ www/js/index.js
@@ -11,11 +11,21 @@ const scaleh = canvas.height/20;
 
 context.scale(scale,scaleh);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
+//const matrix = [];
+
+function arenaSweep() {
+  outer: for (var y = arena.length - 1; y > 0; --y) {
+    for (var x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    ++y;
+  }
+}
 
 function collide(arena, player) {
   //const [m, o] = [player.matrix, player.pos];
@@ -39,6 +49,64 @@ function createMatrix(w, h) {
   return matrix;
 }
 
+//const pieces = 'ILJOTSZ';
+const colors = [
+  null,
+  '#AA00FF',
+  '#FFFF00',
+  '#FFA500',
+  '#0000FF',
+  '#00FFFF',
+  '#00FF00',
+  '#FF0000'
+];
+
+function createPiece(type) {
+  if(type === 'T') {
+    return [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ];
+  } else if (type === 'O') {
+    return [
+      [2, 2],
+      [2, 2],
+    ];
+  } else if (type === 'L') {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3],
+    ];
+  } else if (type === 'J') {
+    return [
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0],
+    ];
+  } else if (type === 'I') {
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+    ];
+  } else if (type === 'S') {
+    return [
+      [0, 0, 0],
+      [0, 6, 6],
+      [6, 6, 0],
+    ];
+  } else if (type === 'Z') {
+    return [
+      [0, 0, 0],
+      [7, 7, 0],
+      [0, 7, 7],
+    ];
+  }
+}
+
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0,0, canvas.width, canvas.height);
@@ -50,8 +118,11 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if(value) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
+
+        context.fillStyle = 'rgba(225,225,225,0.2)';
+        context.fillRect(x + offset.x, y + offset.y, 0.9, 0.9);
       }
     });
   });
@@ -68,8 +139,8 @@ function merge(arena, player) {
 }
 
 const player = {
-  pos: { x:5, y:5},
-  matrix: matrix
+  pos: { x:5, y:0},
+  matrix: getPiece()
 };
 
 function playerDrop() {
@@ -78,6 +149,9 @@ function playerDrop() {
     player.pos.y--;
     merge(arena, player);
     player.pos.y = 0;
+    player.pos.x = 5;
+    playerReset();
+    arenaSweep();
   }
   dropCounter = 0;
 }
@@ -89,8 +163,64 @@ function playerMove(dir) {
   }
 }
 
+function getPiece() {
+  const pieces = 'ILJOTSZ';
+  const piece = pieces[pieces.length * Math.random() | 0];
+  return createPiece(piece);
+}
+
+function playerReset() {
+  player.matrix = getPiece();
+  if(collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+    //alert('perdiste');
+  }
+}
+
+function playerRotate(dir) {
+  let offset = 1;
+  let pos = player.pos.x;
+  rotate(player.matrix, 1);
+
+  if(collide(arena, player)) {
+    rotate(player.matrix, -dir);
+  }
+  // while (collide(arena, player)) {
+  //   player.pos.x += offset;
+  //   offset = -(offset + (offset > 0 ? 1 : -1));
+  //   if(offset > player.matrix[0].length) {
+  //     rotate(player.matrix, -dir);
+  //     player.pos.x = pos;
+  //     return;
+  //   }
+  // }
+}
+
+function rotate(matrix, dir) {
+
+  for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < y; x++) {
+      [
+        matrix[x][y],
+        matrix[y][x]
+      ] = [
+        matrix[y][x],
+        matrix[x][y]
+      ];
+    }
+  }
+
+  if(dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+
+
+}
+
 let dropCounter = 0;
-let dropInterval = 500;
+let dropInterval = 200;
 let lastTime = 0;
 
 function update(time) {
@@ -131,7 +261,7 @@ hammertime.on('swipedown', function(ev) {
 });
 
 hammertime.on('tap', function(ev) {
-	console.log('tap');
+  playerRotate(1);
 });
 
 
